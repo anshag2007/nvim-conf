e14ae60e04e8753d60544625b7116e4324b10190
anshag2007 <anshag2007@gmail.com>
2024-11-27 17:27:51 +0530

    First commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("config.lazy")
+require("config.remap")
+require("config.set")
+vim.g.netrw_browse_split = 0
+vim.g.netrw_winsize = 25
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd.colorscheme "oxocarbon"
---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,16 @@
+
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+            vim.wo.rnu = true
+        end)
+
+
+    end
+}
+
+
