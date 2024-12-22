276a6bc874b8e89f57e4d21adcc15cfb53921f50
Ansh Agarwal <anshag2007@gmail.com>
2024-12-22 14:13:32 +0530

    test

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -23,3 +23,9 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>j", "<C-]>") -- jump to link
 vim.keymap.set("n", "<leader>w", vim.cmd.wa)
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<cr>")
