3b6c355904044502491005704da97f3669a96769
Ansh Agarwal <anshag2007@gmail.com>
2024-12-22 20:07:54 +0530

    minor

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,16 +1,21 @@
-
-return {
+return
+{
     "folke/zen-mode.nvim",
     config = function()
         vim.keymap.set("n", "<leader>zz", function()
-            require("zen-mode").toggle()
+            require("zen-mode").toggle({
+                window = {
+                    width = 1,
+                    height = 1
+                },
+                plugins = {
+                    gitsigns = { enabled = false }, -- disables git signs
+                }
+            })
+            -- vim.cmd('colorscheme github_dark_high_contrast')
             vim.wo.wrap = false
             vim.wo.number = true
             vim.wo.rnu = true
         end)
-
-
     end
 }
-
-
