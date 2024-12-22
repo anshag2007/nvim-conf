27b59fe6e347ac7f9071489d8d4ed2383d293d2d
Ansh Agarwal <anshag2007@gmail.com>
2024-12-22 13:50:09 +0530

    Keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,25 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim'
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim'
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-y>"] = require("telescope.actions").select_default,
+                    },
+                }
+            }
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
     end
-    }
+}
